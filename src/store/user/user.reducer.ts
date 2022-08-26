9332fac2e53d49e52eaa45616d5991df040f1729
Tariq Khan <Khantarikact@gmail.com>
2022-08-26 21:17:40 +0530

    convert user.reducer and user.selector

--- src/store/user/user.reducer.ts
+++ src/store/user/user.reducer.ts
@@ -1,6 +1,5 @@
 import { AnyAction } from 'redux';
 
-import { USER_ACTION_TYPES } from './user.types';
 
 import { signInFailed, signUpFailed, signOutFailed, signOutSuccess, signInSuccess } from './user.action';
 
